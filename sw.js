0d3dc251d2643100b191485e4b427a214e6e4b01
Nhlovo <137711037+Nhlobo@users.noreply.github.com>
2025-10-03 10:19:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,6 +20,19 @@ self.addEventListener("install", (event) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+// Activate SW (optional, but good for cache cleanup)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 // Fetch
